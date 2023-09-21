986bf972937c20ddc826b203847e01ca3dcdc4b1
M Nindra Zaka <mnindrazaka@gmail.com>
2023-09-21 10:57:37 +0700

    chore: tidy up project structure

--- web/__generated__/api/models/index.ts
+++
@@ -1,5 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Product';
-export * from './ProductBody';
-export * from './ProductsGet200Response';
--- web/src/http/index.ts
+++
@@ -1,5 +0,0 @@
-import { DefaultApi, Configuration } from "../../__generated__/api";
-
-export const api = new DefaultApi(
-  new Configuration({ basePath: "https://dummyjson.com" })
-);
